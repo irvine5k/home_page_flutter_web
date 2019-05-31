59575c57c65b812c1832f0ef3b7eced9c798f39f
Kelven <kelvengalvao@gmail.com>
2019-05-31 10:42:20 -0300

    Site concept

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:concept_web/src/pages/home/home_page.dart';
+import 'package:flutter_web/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(home: HomePage());
+}
